6dc241c6f9ffa4d553a7d859c061d1aced3e116e
Try Ajitiono <ballinst@gmail.com>
2020-03-29 23:31:06 +0700

    add TS, refactor Navbar component
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Link } from 'gatsby';
+import github from '../img/github-icon.svg';
+import logo from '../img/logo.svg';
+
+function Navbar() {
+  const [state, setState] = useState({
+    active: false,
+    navBarActiveClass: ''
+  });
+
+  function toggleHamburger() {
+    // toggle the active boolean in the state
+    setState(oldState => ({
+      active: !oldState.active,
+      navBarActiveClass: !oldState.active
+        ? 'is-active'
+        : oldState.navBarActiveClass
+    }));
+  }
+
+  return (
+    <nav
+      className="navbar is-transparent bg-blue-500"
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-item" title="Logo">
+            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          </Link>
+          {/* Hamburger menu */}
+          <div
+            className={`navbar-burger burger ${state.navBarActiveClass}`}
+            data-target="navMenu"
+            onClick={toggleHamburger}
+          >
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+        <div id="navMenu" className={`navbar-menu ${state.navBarActiveClass}`}>
+          <div className="navbar-start has-text-centered">
+            <Link className="navbar-item" to="/about">
+              About
+            </Link>
+            <Link className="navbar-item" to="/products">
+              Products
+            </Link>
+            <Link className="navbar-item" to="/blog">
+              Blog
+            </Link>
+            <Link className="navbar-item" to="/contact">
+              Contact
+            </Link>
+            <Link className="navbar-item" to="/contact/examples">
+              Form Examples
+            </Link>
+          </div>
+          <div className="navbar-end has-text-centered">
+            <a
+              className="navbar-item"
+              href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="icon">
+                <img src={github} alt="Github" />
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
