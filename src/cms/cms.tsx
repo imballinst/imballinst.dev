cf9ce9c503e856b9708b9355335d86a5e413cd2c
Try Ajitiono <ballinst@gmail.com>
2020-04-04 16:01:40 +0700

    woo remove unneeded and fix stuff
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- src/cms/cms.tsx
+++ src/cms/cms.tsx
@@ -1,16 +1,9 @@
 import CMS from 'netlify-cms-app';
-import uploadcare from 'netlify-cms-media-library-uploadcare';
-import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
-import ProductPagePreview from './preview-templates/ProductPagePreview';
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 
-CMS.registerMediaLibrary(uploadcare);
-CMS.registerMediaLibrary(cloudinary);
-
-CMS.registerPreviewTemplate('index', IndexPagePreview);
-CMS.registerPreviewTemplate('about', AboutPagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
-CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('index', IndexPagePreview as any);
+CMS.registerPreviewTemplate('about', AboutPagePreview as any);
+CMS.registerPreviewTemplate('blog', BlogPostPreview as any);
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,4 @@
 declare module '*.svg';
+
+declare module 'netlify-cms-media-library-uploadcare';
+declare module 'netlify-cms-media-library-cloudinary';
