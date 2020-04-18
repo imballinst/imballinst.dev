8f5e70dca0f57a285d8acc531c49eb7f67b4f8b1
Try Ajitiono <ballinst@gmail.com>
2020-04-18 17:40:19 +0700

    change plugin
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,13 @@ module.exports = {
         purgeOnly: ['/all.css']
       }
     },
-    'gatsby-plugin-webpack-bundle-analyzer',
+    [
+      `gatsby-plugin-webpack-size`,
+      {
+        // Set to true to show bundle sizes in development mode as well
+        development: true
+      }
+    ],
     // must be after other CSS plugins
     'gatsby-plugin-netlify' // make sure to keep it last in the array
   ]
