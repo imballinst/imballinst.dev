c1d6c74cf0b50fc298a83465a404c56032e0946c
Try Ajitiono <ballinst@gmail.com>
2020-04-26 18:16:15 +0700

    docs: bugfix on smaller screens (#3)
    This PR updates the max width on the content when the screen size is smaller than 768px.
    
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,11 @@
 import React, { ReactNode } from 'react';
-import { Helmet } from 'react-helmet';
 import styled from '@emotion/styled';
 
 import Navbar from './Navbar';
 import '../all.css';
 import { peepoTheme } from '../theme';
 import SEO from './SEO';
+import { cls } from '../helpers/styles';
 
 // Wrapper for bottom padding. When it is scrollable, the bottom-padding will not be visible.
 export const SectionWrapper = styled.section`
@@ -19,31 +19,41 @@ export const SectionWrapper = styled.section`
 `;
 
 // Layout.
-type Props = {
-  children: ReactNode;
-  seoTitle?: string;
-};
-
 const Content = styled.div`
   height: calc(100vh - ${peepoTheme.topbarHeight}px - 2rem);
   margin-top: ${peepoTheme.topbarHeight}px;
 
   & > * {
-    width: ${peepoTheme.maxOptimalWidth};
+    width: 100%;
   }
 
   @media (min-width: ${peepoTheme.maxOptimalWidth}) {
     height: calc(100vh - ${peepoTheme.topbarHeight}px - 3rem);
+    & > * {
+      width: ${peepoTheme.maxOptimalWidth};
+    }
   }
 `;
 
-function TemplateWrapper({ children, seoTitle }: Props) {
+type Props = {
+  children: ReactNode;
+  seoTitle?: string;
+  noPadding?: boolean;
+};
+
+function TemplateWrapper({ children, seoTitle, noPadding }: Props) {
   return (
     <div>
       <SEO title={seoTitle} />
       <Navbar />
       <Content
-        className={`${peepoTheme.pageVerticalSpacing} ${peepoTheme.pageHorizontalSpacing} flex flex-row justify-center`}
+        className={cls(
+          `${peepoTheme.pageVerticalSpacing} ${peepoTheme.pageHorizontalSpacing} flex flex-row justify-center`,
+          {
+            'px-4': !noPadding,
+            'py-2': !noPadding
+          }
+        )}
       >
         {children}
       </Content>
