29db7c8af4bf40496ae77ada4c43909564c71756
Try Ajitiono <ballinst@gmail.com>
2021-09-21 08:44:45 +0700

    its not productive using vanilla extract with astro
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- snowpack.config.mjs
+++
@@ -1,7 +0,0 @@
-// Example: snowpack.config.mjs
-// The added "@type" comment will enable TypeScript type information via VSCode, etc.
-
-/** @type {import("snowpack").SnowpackUserConfig } */
-export default {
-  plugins: ['@vanilla-extract/snowpack-plugin']
-};
