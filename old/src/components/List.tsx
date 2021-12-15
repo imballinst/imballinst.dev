c315392a53ff4dcdefab2bf8dc70d5d731965c50
Try Ajitiono <ballinst@gmail.com>
2021-12-15 12:52:37 +0700

    nuke them all
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- old/src/components/List.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-import { peepoTheme } from '../theme';
-
-export const ListItem = styled.li`
-  &:not(:first-of-type) {
-    margin-left: ${peepoTheme.spacing(2)};
-  }
-`;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,42 @@
+const colors = require('tailwindcss/colors');
+const config = require('tailwindcss/defaultConfig');
+
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}']
+  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
+      red: colors.red,
+      teal: {
+        50: '#E6FFFA',
+        100: '#B2F5EA',
+        200: '#81E6D9',
+        300: '#4FD1C5',
+        400: '#38B2AC',
+        500: '#319795',
+        600: '#2C7A7B',
+        700: '#285E61',
+        800: '#234E52',
+        900: '#1D4044'
+      }
+    },
+    transitionDuration: {
+      ...config.theme.transitionDuration,
+      250: '250ms',
+      DEFAULT: '250ms'
+    },
+    extend: {
+      width: {
+        wmd: '48rem',
+        wlg: '60rem',
+        wxl: '72rem'
+      }
+    }
+  }
 };
