b0bd5c5ffa1fcf786fd34bfbf37324772cef8461
Try Ajitiono <ballinst@gmail.com>
2021-12-15 19:16:08 +0700

    I was silly to render the stringify
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
+import imageCaptionPlugin from './plugins/image/index.mjs';
+
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
