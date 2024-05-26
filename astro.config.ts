e7d3f7c02a7e8a0c92baff6221952143952e2aa9
Try Ajitiono <ballinst@gmail.com>
2024-05-26 15:44:36 +0000

    prettify config

--- astro.config.ts
+++ astro.config.ts
@@ -3,15 +3,21 @@ import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 import imageCaptionPlugin from './plugins/image/index.mjs';
 import htmlClassnamesPlugin from './plugins/html-classnames/index.mjs';
-
-import expressiveCode from "astro-expressive-code";
+import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind({
-    applyBaseStyles: false
-  }), expressiveCode()],
-  site: process.env.CONTEXT === 'production' ? process.env.URL : process.env.DEPLOY_PRIME_URL,
+  integrations: [
+    preact(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    expressiveCode()
+  ],
+  site:
+    process.env.CONTEXT === 'production'
+      ? process.env.URL
+      : process.env.DEPLOY_PRIME_URL,
   markdown: {
     remarkPlugins: [imageCaptionPlugin, htmlClassnamesPlugin],
     rehypePlugins: []
